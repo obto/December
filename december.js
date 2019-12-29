1a908b5f40e2dba85fcab0ab305149a38b7944d8
HappyHub1 <vlee9680@hotmail.com>
2019-12-28 23:14:48 -0500

    Change Favicon URL

--- december.js
+++ december.js
@@ -34,7 +34,7 @@ var advertisements = [
 
 var adPercent = 0.1;
 
-var Favicon_URL = 'https://files.catbox.moe/6sr1me.png';
+var Favicon_URL = 'https://cdn.jsdelivr.net/gh/HappyHub1/December/tiger.png';
 
 var ChannelName_Caption = '25 Days of Autism';
 
