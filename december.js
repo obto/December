591b5492b52de71b335637090e5107ccd3cac9a3
ShizuruAnon <shizuruanon@gmail.com>
2020-11-26 23:05:13 -0500

    bugfix

--- december.js
+++ december.js
@@ -3389,7 +3389,7 @@ class CustomTextTriggers {
         }
 
         const [effect_name, ...effect_args] = message_parts;
-        if (command_name[0] !== '/') {
+        if (effect_name !== '/') {
             return;
         }
 
