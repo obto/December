b29bc1c582fb1c20c7c8509ccc98bdcfeb169796
David Fox <david@davidjfox.com>
2020-11-29 20:35:32 -0600

    Disable the snow effect until the new version has been IMPLd

--- december.js
+++ december.js
@@ -3384,7 +3384,7 @@ Then add it to the `effects` static variable below
 class CustomTextTriggers {
 
     // Only place you need to add a new effect to make it work
-    static effects = [ErabeEffect, SnowEffect, PadoruEffect, PresentsEffect];
+    static effects = [ErabeEffect, /*SnowEffect, */PadoruEffect, PresentsEffect];
 
     static init() {
         if (CustomTextTriggers.has_init) {
