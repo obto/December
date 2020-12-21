cb43806d5e1d20f57e4e8107c880fe7b5766abec
David Fox <david@davidjfox.com>
2020-12-21 15:30:09 -0600

    Real space heater

--- december.js
+++ december.js
@@ -4156,7 +4156,7 @@ SnowEffect.snow_levels = {
   high: {min: 1000, max: 3500},
   blizzard: {min: 500, max: 5000},
   prime95: {min: 75, max: 900},
-  space_heater: {min: 35, max: 450},
+  space_heater: {min: 30, max: 400},
 };
 SnowEffect.vertex_shader_src = `
 	attribute vec2 a_position;
